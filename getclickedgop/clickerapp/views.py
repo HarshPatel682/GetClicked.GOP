47bee45e6db5f0be510b44dc1c6a696252bed7a0
Daniel Ostrowski <daniel.j.ostrowski@gmail.com>
2017-11-16 03:05:02 +0000

    Corrects testpage usable for testing

--- getclickedgop/clickerapp/views.py
+++ getclickedgop/clickerapp/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import cgi
 
 def index(request):
     return HttpResponse("Welcome to GetClicked.GOP!")
 
 def testpost(request):
-    text = "This is what the server got as part of the POST:\n" + str(request.POST)
+    text = "This is what the server got as part of the POST:\n" + cgi.escape(str(request.POST))
     return HttpResponse(text)
