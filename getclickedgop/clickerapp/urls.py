1b54b6beec3e94a4370c0a9e966956d8f5661a76
Daniel Ostrowski <daniel.j.ostrowski@gmail.com>
2017-11-23 23:09:15 +0000

    Implements create user/login/logout on server - see Google Doc
    Implements "pages" that can be posted to in order to create a user, log a user in, and log a user out. Please see the Google Doc for a more detailed explanation.

--- getclickedgop/clickerapp/urls.py
+++ getclickedgop/clickerapp/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^testpost/$', views.testpost, name='testpost'),
-    url(r'^deploy/$', views.deploy, name='deploy')
+    url(r'^createuser', views.createuser, name='createuser'),
+    url(r'^loginuser', views.loginuser, name='loginuser'),
+    url(r'^logoutuser', views.logoutuser, name='logoutuser'),
+    url(r'^deploy', views.deploy, name='deploy')
 ]
--- getclickedgop/clickerapp/views.py
+++ getclickedgop/clickerapp/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse
 import cgi
 import os
 import subprocess
+import json
+from django.contrib.auth import authenticate, login, logout
+from models import *
 
 def index(request):
     return HttpResponse("Welcome to GetClicked.GOP!")
@@ -15,5 +18,56 @@ def testpost(request):
     return HttpResponse(text)
 
 def deploy(request):
-    text = subprocess.check_output('touch /home/bitnami/public/DEPLOY', stderr=subprocess.STDOUT, shell=True)
+    #text = subprocess.check_output('touch /home/bitnami/public/DEPLOY', stderr=subprocess.STDOUT, shell=True)
+    text = subprocess.check_output('w', stderr=subprocess.STDOUT, shell=True)
     return HttpResponse(text)
+
+def createuser(request):
+    result = {}
+    if "username" not in request.POST:
+        result["success"] = False
+        result["comment"] = "A username was not supplied."
+        return HttpResponse(json.dumps(result))
+    elif "password" not in request.POST:
+        result["success"] = False
+        result["comment"] = "A password was not supplied."
+        return HttpResponse(json.dumps(result))
+    username = request.POST["username"]
+    password = request.POST["password"]
+    if len(User.objects.filter(username=username)) != 0:
+        result["success"] = False
+        result["comment"] = "That username is already taken."
+        return HttpResponse(json.dumps(result))
+    User.objects.create_user(username, None, password)
+    result["success"] = True
+    result["comment"] = "A new user was successfully created."
+    return HttpResponse(json.dumps(result))
+
+def loginuser(request):
+    result = {}
+    if "username" not in request.POST:
+        result["success"] = False
+        result["comment"] = "A username was not supplied."
+        return HttpResponse(json.dumps(result))
+    elif "password" not in request.POST:
+        result["success"] = False
+        result["comment"] = "A password was not supplied."
+        return HttpResponse(json.dumps(result))
+    username = request.POST["username"]
+    password = request.POST["password"]
+    user = authenticate(username=username, password=password)
+    if user is None:
+        result["success"] = False
+        result["comment"] = "The username and password combination was not recognized."
+        return HttpResponse(json.dumps(result))
+    else:
+        result["success"] = True
+        result["comment"] = "The user has been logged in successfully."
+        login(request, user)
+        return HttpResponse(json.dumps(result))
+
+def logoutuser(request):
+    result = {}
+    logout(request)
+    result["success"] = True
+    return HttpResponse(json.dumps(result))
